2dc9a5d1ffbc6cc6d99d4284f306968fecb0900d
Amrit Kumar Yadav <kittyamrit005@gmail.com>
2024-08-02 11:32:48 +0530

    create 64.js create 64_chaining_promises.html

---
+++ 64.js
@@ -0,0 +1,19 @@
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        console.log("Resolve after 2 seconds")
+        resolve(1)
+    }, 2000);
+})
+
+p1.then((value)=>{                             // .then() for p1
+    console.log(value)
+    let p2 = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("Resolve p2")
+            resolve(2)
+        },2000)
+    })
+    return p2
+}).then((value)=>{                         // .then() for p2
+    console.log(value)       
+})
